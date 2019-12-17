6e26f988006d531f8ecf88655c509a7de66a5869
wpengc <wangpc@yinhechuangxiang.com>
2019-12-17 10:54:23 +0800

    加了一行打印代码

--- src/main/java/com/wpc/controller/TestController.java
+++ src/main/java/com/wpc/controller/TestController.java
@@ -32,6 +32,7 @@ public class TestController {
         System.out.println("22222222222222");
         System.out.println("11111111111111");
         System.out.println("11111111111111");
+        System.out.println(333333333333333);
         return list;
     }
 
